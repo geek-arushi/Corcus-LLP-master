3b00f671d9d4022c4c5a59a20e4eefe6d8affcc2
geek-arushi <arushisingh1121@gmail.com>
2025-04-04 22:03:34 +0530

    arushi

--- src/Components/Header/Nav.jsx
+++ src/Components/Header/Nav.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function Nav({ setMobileToggle }) {
   return (
-    <ul className="cs_nav_list fw-medium space-y-4 sm:space-y-0">
+    <ul className="cs_nav_list fw-medium space-y-2 sm:space-y-0">
       <li className="menu-item transition-all duration-300 hover:opacity-80">
-        <Link to="/" className="block py-2 px-4">
+        <Link to="/" className="block py-1 px-2">
           Home
         </Link>
       </li>
       <li className="menu-item transition-all duration-300 hover:opacity-80">
-        <Link to="/about" className="block py-2 px-4">
+        <Link to="/about" className="block py-1 px-2">
           About Us
         </Link>
       </li>
@@ -18,7 +18,7 @@ export default function Nav({ setMobileToggle }) {
         <Link
           to="/service"
           onClick={() => setMobileToggle(false)}
-          className="block py-2 px-4"
+          className="block py-1 px-2"
         >
           Services
         </Link>
@@ -27,7 +27,7 @@ export default function Nav({ setMobileToggle }) {
         <Link
           to="/contact"
           onClick={() => setMobileToggle(false)}
-          className="block py-2 px-4"
+          className="block py-1 px-2"
         >
           Contact US
         </Link>
