81387b8b7dc2ca4e9296044acb45ed06b8379761
geek-arushi <arushisingh1121@gmail.com>
2025-04-04 10:24:30 +0530

    arushi

--- src/Layout/Layout2.jsx
+++ src/Layout/Layout2.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header2 from "../Components/Header/Header2";
 import Footer2 from "../Components/Footer/Footer2";
 import WhatsAppButton from "../Components/WhatsAppButton";
+import CustomCursor from "../Components/CustomCursor";
 const Layout2 = () => {
   return (
     <div className="main-page-area2">
@@ -9,6 +10,7 @@ const Layout2 = () => {
       <Outlet></Outlet>
       <Footer2></Footer2>
       <WhatsAppButton />
+      <CustomCursor />
     </div>
   );
 };
