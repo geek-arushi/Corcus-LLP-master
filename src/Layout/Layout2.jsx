fa8673d2d687ac120012ea1290fa99020e26da70
geek-arushi <arushisingh1121@gmail.com>
2025-04-10 19:11:02 +0530

    arushi ji

--- src/Layout/Layout2.jsx
+++ src/Layout/Layout2.jsx
@@ -3,9 +3,11 @@ import Header2 from "../Components/Header/Header2";
 import Footer2 from "../Components/Footer/Footer2";
 import WhatsAppButton from "../Components/WhatsAppButton";
 import CustomCursor from "../Components/CustomCursor";
+import ScrollToTop from "../Components/Common/ScrollToTop";
 const Layout2 = () => {
   return (
     <div className="main-page-area2">
+      <ScrollToTop />
       <Header2></Header2>
       <Outlet></Outlet>
       <Footer2></Footer2>
