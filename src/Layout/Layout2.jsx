c19a931222113393b71be54bb645951faf8b706c
geek-arushi <arushisingh1121@gmail.com>
2025-04-20 10:45:45 +0530

    avishek

--- src/Layout/Layout2.jsx
+++ src/Layout/Layout2.jsx
@@ -5,6 +5,7 @@ import WhatsAppButton from "../Components/WhatsAppButton";
 import CustomCursor from "../Components/CustomCursor";
 import ScrollToTop from "../Components/Common/ScrollToTop";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const Layout2 = () => {
   return (
     <div className="main-page-area2">
@@ -14,6 +15,7 @@ const Layout2 = () => {
       <Footer2></Footer2>
       <WhatsAppButton></WhatsAppButton>
       <Analytics></Analytics>
+      <SpeedInsights></SpeedInsights>
       <CustomCursor></CustomCursor>
     </div>
   );
