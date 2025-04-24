6b987301da69e845d6bf6db186bfe4be7e149c32
arushi <arushisingh1121@gmail.com>
2025-04-24 09:34:17 +0530

    aarushi

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -14,6 +14,7 @@ import CaseStudyDetailsPage from "../Pages/CaseStudyDetailsPage";
 import BlogRightSidebar from "../Pages/BlogRightSidebar";
 import BlogPage from "../Pages/BlogPage";
 import BlogDetailsPage from "../Pages/BlogDetailsPage";
+import TermsAndConditions from "../Components/Terms and Condition";
 
 
 export const router = createBrowserRouter([
@@ -53,6 +54,10 @@ export const router = createBrowserRouter([
       {
         path: "/service",
         element: <ServicesPage></ServicesPage>,
+      },
+      {
+        path: "/TermsAndConditions",
+        element: <TermsAndConditions></TermsAndConditions>,
       },
       // {
       //   path: "/",
