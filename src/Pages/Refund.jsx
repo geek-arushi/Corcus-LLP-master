5f78729a6f22293022766dfe72d1a0f4d02b690b
arushi <arushisingh1121@gmail.com>
2025-05-05 09:55:41 +0530

    aaa

--- src/Pages/Refund.jsx
+++ src/Pages/Refund.jsx
@@ -4,7 +4,7 @@ import BreadCumb from "../Components/Common/BreadCumb";
 const Refund = () => {
   return (
     <div>
-      <BreadCumb Title="RefundPolicy"></BreadCumb>
+      <BreadCumb Title="Refund Policy"></BreadCumb>
       <RefundPolicy></RefundPolicy>
     </div>
   );
